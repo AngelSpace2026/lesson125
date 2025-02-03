3c0cb93b3c2d4a17b6dd256253426971107376b7
Jurijus Pacalovas <j.pacalovas@mail.ru>
2025-02-03 12:04:52 +0000

    Add files via upload

--- Q1.py
+++ Q1.py
@@ -5,6 +5,7 @@ while count_number!=(2**25)-1:
 	count_number = int(count_number)
 	QuantumCircuit(count_number)
 	count_number+=1
-	count_number_save=-1
+	count_number_save=count_number
+	count_number_save-=1
 	#print(count_number_save)
                                            
