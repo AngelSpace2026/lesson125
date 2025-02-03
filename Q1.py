4dcdac4f84b9e02cca8f024d85a05314d955bf9f
Jurijus Pacalovas <j.pacalovas@mail.ru>
2025-02-03 12:39:11 +0000

    Add files via upload

--- Q1.py
+++ Q1.py
@@ -1,7 +1,7 @@
 from qiskit import QuantumCircuit
 circuit = QuantumCircuit(26)
 count_number=0
-while count_number!=(2**25)-1:
+while count_number!=(2**25):
 	count_number = int(count_number)
 	QuantumCircuit(count_number)
 	count_number+=1
