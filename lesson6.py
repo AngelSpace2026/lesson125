06f34ee84d0977b648ac92d5037ce00b8675c39f
Jurijus Pacalovas <j.pacalovas@mail.ru>
2025-02-03 10:27:06 +0000

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,8 @@
+print(r'Hello')#raw print
+print(r'Hello')
+print('Hello\n')
+print(r'Line 1\nLine 2\nLine 3')
+print(r'Line 1\nLine 2\nLine 3')
+print(r'Line 1\nLine 2\nLine 3')
+print('print("Hello world")')
+print("Hello"+" "+"Jenny")
