0cca93edd9ba4f6630541022bf5287776b8538f6
Jurijus Pacalovas <j.pacalovas@mail.ru>
2025-02-04 13:13:20 +0000

    Add files via upload

---
+++ Lesson 11.py
@@ -0,0 +1,10 @@
+import os
+a = 22
+b = 34
+print("value " + str(a))  # This prints "value22"
+print("value " + str(b))  # Corrected: Now prints "value34"
+c = a + b
+print(c)  # Prints 56
+float(c)
+float(b)
+print(c/b)
