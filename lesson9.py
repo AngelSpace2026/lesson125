c6f2967d2e7bccce14b0bffb7a1c66ea05ad8281
Jurijus Pacalovas <j.pacalovas@mail.ru>
2025-02-04 12:31:08 +0000

    Add files via upload

---
+++ lesson9.py
@@ -0,0 +1,15 @@
+a = 22
+b = 34
+
+Jenny_Hello = input("Hello! ")
+Yura = "Hello! How are you?"
+Jenny = "I am well!"
+print(Yura + " " + Jenny)
+
+name = input("What is your name? ")
+answer_for_name_is_Jenny = " and me Jenny"
+
+print("My name is " + str(name))
+print(answer_for_name_is_Jenny)
+print("How old are you? " + str(a))
+print("And me is " + str(b))
