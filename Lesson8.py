33b56c7cc2780065d4fd842b2d624227e1f462fb
Jurijus Pacalovas <j.pacalovas@mail.ru>
2025-02-04 11:45:43 +0000

    Add files via upload

---
+++ Lesson8.py
@@ -0,0 +1,8 @@
+import os
+Jenny_Hello=input("Hello! ")
+Yura="Hello! How are you?"
+Jenny="I am well!"
+print(Yura+" "+Jenny)
+name=input("What is your name? ")
+anwer_for_name_is_Jenny="Jenny"
+print("My name is " + str(name))
